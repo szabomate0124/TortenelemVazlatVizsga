5956115282303da0486eecca13899c1a3e5f11ac
devilmate101 <126233986+szabomate0124@users.noreply.github.com>
2026-01-07 18:00:46 +0100

    Főoldal komponens létrehozva

--- TortenelmVazlat/src/main.jsx
+++ TortenelmVazlat/src/main.jsx
@@ -1,10 +1,13 @@
+import 'bootstrap/dist/css/bootstrap.min.css' 
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
+import Fooldal from  './Fooldal'
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <Fooldal />
+
   </StrictMode>,
 )
