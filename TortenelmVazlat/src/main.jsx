1db55756e36887cffee75694d3c1650ac0f6a5d0
devilmate101 <126233986+szabomate0124@users.noreply.github.com>
2026-01-12 14:04:29 +0100

    A bejelentkezés és a regisztrációs oldal elkészűlt funkciója összekapcsolva a főoldallal

--- TortenelmVazlat/src/main.jsx
+++ TortenelmVazlat/src/main.jsx
@@ -1,13 +1,22 @@
-import 'bootstrap/dist/css/bootstrap.min.css' 
+import 'bootstrap/dist/css/bootstrap.min.css'
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
-import Fooldal from  './Fooldal'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
+import './index.css'
+
+import Fooldal from './Fooldal'
+import Login from './Login'
+import Register from './Register'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Fooldal />
-
-  </StrictMode>,
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Fooldal />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+      </Routes>
+    </BrowserRouter>
+  </StrictMode>
 )
