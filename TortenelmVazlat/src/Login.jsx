a6012a14353f3b884b7d72b10173f94b76d13b0e
devilmate101 <126233986+szabomate0124@users.noreply.github.com>
2026-01-14 18:04:34 +0100

    A bejelentkezés és a regisztrációs oldal munkálatainak megkezdése

--- TortenelmVazlat/src/Login.jsx
+++ TortenelmVazlat/src/Login.jsx
@@ -1,6 +1,7 @@
 import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
 import { useNavigate } from "react-router-dom";
+import { useState } from "react";
 import "./auth.css";
 
 function Login() {
