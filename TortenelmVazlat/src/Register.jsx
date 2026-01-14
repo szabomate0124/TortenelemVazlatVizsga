b1ff97f5c3de2f8122add2376743aee58378d61b
devilmate101 <126233986+szabomate0124@users.noreply.github.com>
2026-01-14 18:04:34 +0100

    A bejelentkezés és a regisztrációs oldal munkálatainak megkezdése

--- TortenelmVazlat/src/Register.jsx
+++ TortenelmVazlat/src/Register.jsx
@@ -1,10 +1,30 @@
 import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
 import { useNavigate } from "react-router-dom";
+import { useState } from "react";
 import "./auth.css";
 
 function Register() {
-  const navigate = useNavigate();
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+
+  function handleEmailChange(e) {
+    setEmail(e.target.value);
+  }
+
+  function handlePasswordChange(e) {
+    setPassword(e.target.value);
+  }
+
+  function handleConfirmPasswordChange(e) {
+    setConfirmPassword(e.target.value);
+  }
+
+  function handleSubmit(e) {
+    
+  }
+
 
   return (
     <div className="auth-page">
@@ -14,27 +34,38 @@ function Register() {
         <Form>
           <Form.Group className="mb-3">
             <Form.Label>Email</Form.Label>
-            <Form.Control />
+            <Form.Control value={email} onChange={handleEmailChange} />
           </Form.Group>
 
           <Form.Group className="mb-3">
             <Form.Label>Jelszó</Form.Label>
-            <Form.Control type="password" />
+            <Form.Control
+              type="password"
+              value={password}
+              onChange={handlePasswordChange}
+            />
           </Form.Group>
 
           <Form.Group className="mb-3">
             <Form.Label>Jelszó megerősítése</Form.Label>
-            <Form.Control type="password" />
+            <Form.Control
+              type="password"
+              value={confirmPassword}
+              onChange={handleConfirmPasswordChange}
+            />
           </Form.Group>
 
-          <Button className="auth-btn w-100" onClick={() => navigate("/login")}>
+          <Button className="auth-btn w-100" onClick={handleSubmit}>
             Fiók létrehozása
           </Button>
         </Form>
 
         <div className="auth-footer">
           Már van fiókod?{" "}
-          <span onClick={() => navigate("/login")} style={{ cursor: "pointer" }}>
+          <span
+            onClick={() => navigate("/login")}
+            style={{ cursor: "pointer" }}
+          >
             Bejelentkezés
           </span>
         </div>
