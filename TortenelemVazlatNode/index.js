3fd749d210262c4fed162a113d78c822964cdf69
Dominikzsupos <zsuposdominik@gmail.com>
2026-04-04 13:17:14 +0200

    Delete létrehozása

--- TortenelemVazlatNode/index.js
+++ TortenelemVazlatNode/index.js
@@ -3,20 +3,19 @@ const mysql = require("mysql2");
 const cors = require("cors");
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
-const { randomUUID } = require("crypto"); 
+const { randomUUID } = require("crypto");
 const fs = require("fs");
-const path = require("path"); 
-const multer = require('multer'); 
-
+const path = require("path");
+const multer = require('multer');
 
 const app = express();
 const PORT = 3000;
 const SECRET_KEY = "test";
+
 app.use(cors());
-app.use(express.json());
-app.use("/Torivazlatkepek", express.static("../Torivazlatkepek"));
 app.use(express.json({ limit: "10mb" }));
-app.use(express.urlencoded({ limit: "10mb", extended: true })); 
+app.use(express.urlencoded({ limit: "10mb", extended: true }));
+app.use("/Torivazlatkepek", express.static("../Torivazlatkepek"));
 
 const connection = mysql.createConnection({
   host: "localhost",
@@ -28,7 +27,7 @@ const connection = mysql.createConnection({
 
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
-        cb(null, '../Torivazlatkepek/'); 
+        cb(null, '../Torivazlatkepek/');
     },
     filename: function (req, file, cb) {
         const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9);
@@ -36,12 +35,11 @@ const storage = multer.diskStorage({
     }
 });
 
-const upload = multer({ 
+
+const upload = multer({
     storage: storage,
-    limits: { fileSize: 5 * 1024 * 1024 }, 
+    limits: { fileSize: 5 * 1024 * 1024 },
     fileFilter: (req, file, cb) => {
-        console.log(file)
-        console.log(req)
         if (file.mimetype.startsWith('image/')) {
             cb(null, true);
         } else {
@@ -49,18 +47,17 @@ const upload = multer({
         }
     }
 });
-
-
-
 app.get('/api/topicsByCategory/:id', (req, res)=>{
     const id = req.params.id;
-    const sql = `SELECT DISTINCT topics.id, topics.title, topics.img from topics WHERE topics.category_id = ${id}`;
-    connection.query(sql, (error, results) =>{
+
+    const sql = `SELECT id, title, img FROM topics WHERE category_id = ?`;
+
+    connection.query(sql, [id], (error, results) =>{
         if (error){
             console.error('hiba a lekérdezés során:', error);
             return res.status(500).json({ error: 'Adatbázis hiba'});
-
         }
+
         if(results.length === 0){
             return res.status(404).json({ message: 'Nem található ez a category'})
         }
@@ -69,36 +66,33 @@ app.get('/api/topicsByCategory/:id', (req, res)=>{
     });
 });
 
-
 app.get('/api/content/:catId/:tpcId', (req, res)=>{
     const {tpcId, catId} = req.params;
-    const sql = `SELECT  topics.title, topics.content FROM topics WHERE topics.category_id = ${catId} AND topics.id = ${tpcId}`;
-    connection.query(sql, [tpcId, catId], (error, results) =>{
+
+    const sql = `
+      SELECT title, content FROM topics WHERE category_id = ? AND id = ? `;
+
+    connection.query(sql, [catId, tpcId], (error, results) =>{
         if (error){
             console.error('hiba a lekérdezés során:', error);
             return res.status(500).json({ error: 'Adatbázis hiba'});
         }
+
         if(results.length === 0){
-            return res.status(404).json({ message: 'Nem található ez a category'})
+            return res.status(404).json({ message: 'Nem található ez a téma'})
         }
 
         res.json(results[0]);
     });
 });
 
-//így tudod meghívni fetchben,  nyílván nem kötelező így meghívni a value-t = ${search}
-////---------http://localhost:3000/api/search?q=${search}------\\\\\\\
 app.get("/api/search", (req, res) => {
 
-  const query = req.query.q;
+  const query = req.query.q || "";
 
-  const sql = `
-    SELECT id, title, img, category_id
-    FROM topics
-    WHERE title LIKE ?
-  `;
+  const sql = ` SELECT id, title, img, category_id FROM topics WHERE title LIKE ? `;
 
-  connection.query(sql, [`%${query}%`, `%${query}%`], (err, results) => {
+  connection.query(sql, [`%${query}%`], (err, results) => {
 
     if (err) {
       console.error(err);
@@ -117,39 +111,38 @@ app.post('/upload', upload.single('file'), (req, res) => {
     }
 
     const fileUrl = `http://localhost:${PORT}/Torivazlatkepek/${req.file.filename}`;
-    
     res.json({ location: fileUrl });
 });
 
-
-//új téma hozzáadása
-// a category_id-t ugyanúgy a weboldalról szürje ne az adminnak kelljen megadnia
 app.post('/api/insert', (req, res) => {
 
     const base64Image = req.body.img;
+    let fileName = null;
 
-    if (!base64Image) {
-        return res.status(400).send("No image provided");
+    if (base64Image) {
+        const base64Data = base64Image.replace(/^data:image\/\w+;base64,/, "");
+        fileName = randomUUID() + ".png";
+        const savePath = path.join(__dirname, "../Torivazlatkepek", fileName);
+        fs.writeFileSync(savePath, Buffer.from(base64Data, "base64"));
     }
 
-    const base64Data = base64Image.replace(/^data:image\/\w+;base64,/, "");
-    const fileName = randomUUID() + ".png";
-    const savePath = path.join(__dirname, "../Torivazlatkepek", fileName);
-    fs.writeFileSync(savePath, Buffer.from(base64Data, "base64")); 
+    const { title, category_id, content } = req.body;
 
-    const { title, category_id, content, img } = req.body;
+    const sqlInsert = `
+      INSERT INTO topics (title, category_id, content, img) VALUES (?, ?, ?, ?) `;
 
-    const sqlInsert = "INSERT INTO topics (title, category_id, content, img) VALUES (?,?,?,?)";
-    connection.query(sqlInsert, [title, category_id, content, fileName], (err, result) => {
+    connection.query(
+      sqlInsert,
+      [title, category_id, content, fileName],
+      (err) => {
         if (err) {
             console.log(err);
-            res.status(500).send({message: "Hiba a mentés során!"} );
-        } else {
-            res.send({message: "Sikeres mentés"} );
+            return res.status(500).send({message: "Hiba a mentés során!"});
         }
-    });
+        res.send({message: "Sikeres mentés"});
+      }
+    );
 });
-
 
 app.post("/api/register", async (req, res) => {
     const { username, email, password } = req.body;
@@ -162,7 +155,7 @@ app.post("/api/register", async (req, res) => {
         const hashedPassword = await bcrypt.hash(password, 10);
 
         const sql = "INSERT INTO users (username, email, password, auth_id) VALUES (?, ?, ?, 2)";
-        connection.query(sql, [username, email, hashedPassword], (err, results) => {
+        connection.query(sql, [username, email, hashedPassword], (err) => {
             if (err) {
                 console.error("Adatbázis hiba:", err);
                 return res.status(500).json({ error: "Adatbázis hiba" });
@@ -174,7 +167,6 @@ app.post("/api/register", async (req, res) => {
         res.status(500).json({ error: "Szerver hiba" });
     }
 });
-
 
 app.post("/api/login", (req, res) => {
     const { email, password } = req.body;
@@ -200,12 +192,16 @@ app.post("/api/login", (req, res) => {
         if (!match) {
             return res.status(401).json({ error: "Helytelen email vagy jelszó" });
         }
-        console.log(user)
-        const token = jwt.sign({ id: user.id, username: user.username, email: user.email, auth_id: user.auth_id }, SECRET_KEY, { expiresIn: "1h" });
+
+        const token = jwt.sign(
+          { id: user.id, username: user.username, email: user.email, auth_id: user.auth_id },
+          SECRET_KEY,
+          { expiresIn: "1h" }
+        );
+
         res.json({ message: "Sikeres bejelentkezés", token });
     });
 });
-
 
 const authenticateToken = (req, res, next) => {
     const authHeader = req.headers["authorization"];
@@ -220,9 +216,6 @@ const authenticateToken = (req, res, next) => {
     });
 };
 
-
-
-//admin features
 app.put("/api/topics/:catId/:tpcId", authenticateToken, (req, res) => {
   const { catId, tpcId } = req.params;
   const { title, content } = req.body;
@@ -231,11 +224,7 @@ app.put("/api/topics/:catId/:tpcId", authenticateToken, (req, res) => {
     return res.status(403).json({ error: "Nincs jogosultság" });
   }
 
-  const sql = `
-    UPDATE topics
-    SET title = ?, content = ?
-    WHERE id = ? AND category_id = ?
-  `;
+  const sql = ` UPDATE topics SET title = ?, content = ? WHERE id = ? AND category_id = ?`;
 
   connection.query(sql, [title, content, tpcId, catId], (err, results) => {
     if (err) {
@@ -251,14 +240,29 @@ app.put("/api/topics/:catId/:tpcId", authenticateToken, (req, res) => {
   });
 });
 
+//téma törlése
+app.delete("/api/topics/:catId/:tpcId", authenticateToken, (req, res) => {
+  const { catId, tpcId } = req.params;
 
+  if (req.user.auth_id !== 1) {
+    return res.status(403).json({ error: "Nincs jogosultság" });
+  }
 
+  const sql = `DELETE FROM topics WHERE topics.id = ? AND topics.category_id = ?`;
 
+  connection.query(sql, [tpcId, catId], (err, results) => {
+    if (err) {
+      console.error(err);
+      return res.status(500).json({ error: "Adatbázis hiba" });
+    }
 
+    if (results.affectedRows === 0) {
+      return res.status(404).json({ error: "Nem található a téma" });
+    }
 
-
-
-
+    res.json({ message: "Sikeres törlés" });
+  });
+});
 
 
 
